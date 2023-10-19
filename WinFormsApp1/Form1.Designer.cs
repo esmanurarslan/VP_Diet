9d95b8bdb6f6ad96b96a3fedc2071c10c4401060
Esma Nur Arslan <esmanurarslan2016@gmail.com>
2023-10-19 20:34:07 +0300

    Proje dosyası ekle.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(20, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(377, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Ne Olarak Giriş Yapmak İstersiniz?";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            button1.AutoSize = true;
+            button1.BackColor = Color.MediumAquamarine;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(72, 89);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 47);
+            button1.TabIndex = 1;
+            button1.Text = "Diyetisyen";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            button2.AutoSize = true;
+            button2.BackColor = Color.MediumAquamarine;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(226, 89);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 47);
+            button2.TabIndex = 2;
+            button2.Text = "Danışan";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(44, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(411, 178);
+            panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumAquamarine;
+            ClientSize = new Size(514, 262);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Giriş Paneli";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+    }
+}
