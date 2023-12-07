ca019e286238dfd135e13ef314b00775f18de323
SevvalAycan <aycan.sevval2@gmail.com>
2023-12-08 00:22:43 +0300

    Danişan anasayfa ve güncelleme sayfaları hazırlandı

--- WinFormsApp1/DanisanPanel.cs
+++ WinFormsApp1/DanisanPanel.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+
 namespace WinFormsApp1
 {
     public partial class DanisanPanel : Form
@@ -15,6 +18,34 @@ namespace WinFormsApp1
         public DanisanPanel()
         {
             InitializeComponent();
+        }
+
+        
+
+        private void grafikPanel_Paint(object sender, PaintEventArgs e)
+        {
+            /*Örnek veri seti (kilo ve zaman verileri)
+           // DateTime[] dates = { DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-4), DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-2), DateTime.Now.AddDays(-1), DateTime.Now };
+            double[] weights = { 70.5, 71.2, 69.8, 72.0, 70.7, 73.2 };
+
+            // Chart kontrolüne verileri ekleme
+            for (int i = 0; i < dates.Length; i++)
+            {
+                chart1.Series["Weight"].Points.AddXY(dates[i], weights[i]);
+            }
+
+            // Eksen başlıkları ve grafiğin başlığı
+            chart1.ChartAreas[0].AxisX.Title = "Tarih";
+            chart1.ChartAreas[0].AxisY.Title = "Kilo (kg)";
+            chart1.Titles.Add("Kilo Zaman Grafiği"); */
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Guncelle frm = new Guncelle();
+            frm.Show();
         }
     }
 }
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Guncelle : Form
+    {
+
+        private List<double> kiloListesi = new List<double>();
+        public Guncelle()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Guncelle_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double kilo))
+            {
+                kiloListesi.Add(kilo);
+                textBox1.Clear();
+                MessageBox.Show("Güncelleme Başarılı", "Sonuç", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                MessageBox.Show("Geçerli bir kilo değeri giriniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
